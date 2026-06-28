7f3065aaddaa47e8c02dd9cdb42fd3e96d4dc364
AkinyA <nyaraja398@gmail.com>
2026-06-28 19:43:47 +0300

    Update configApi.js

--- dolibarr_eval_serie3/src/api/configApi.js
+++ dolibarr_eval_serie3/src/api/configApi.js
@@ -5,7 +5,7 @@ export const API_CONFIG = {
   BASE_URL: 'http://localhost:1280/dolibarr-23.0.3/htdocs/api/index.php',
   
   // Clé API à récupérer dans Dolibarr (Configuration -> Modules -> API/Webservices -> Clé de l'utilisateur)
-  API_KEY: '14c913c113f05a0c794d142e9afb005458320c1a', 
+  API_KEY: '6d07f89ba97c19a51d0244e1bc510a3fc5c181b9', 
   
   // Code unique pour l'accès au Backoffice de la NewAPP
   BACKOFFICE_CODE: 'Nykia39800'
